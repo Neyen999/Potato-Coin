3eef63dcbf347016e6a59669e4dcdaaa34054710
Neyen Marinelli <neyen_gt@hotmail.com>
2020-10-26 14:51:33 -0300

    Plan Section Structure

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.main__tables--container', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-100px',
+})
+
+sr.reveal('.benefits', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-100px',
+})
